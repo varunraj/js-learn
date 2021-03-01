0f6f3fd8e21b44594e9d98fdd71b35bb53dd60d6
Varun Raj <varunraj82@gmail.com>
2021-03-01 14:09:53 -0500

    added oops 1

--- xx_advanced_js/04 OOP/4 inheritance.js
+++ xx_advanced_js/04 OOP/4 inheritance.js
@@ -1,5 +1,6 @@
 class Character {
 
+
     constructor(name, weapon) {
         this.name = name;
         this.weapon = weapon;
@@ -36,4 +37,25 @@ class Ogre extends Character {
 
 const shrek = new Ogre('shrek', 'club', 'green')
 shrek.makeFort()
+
+Ogre.prototype.isPrototypeOf(shrek) // true
+Character.prototype.isPrototypeOf(Ogre.prototype) // true
+
+// instanceOf
+
+dolby instanceof (Elf) // true
+dolby instanceof (Ogre) // false
+dolby instanceof (Character) // true
+
+// we dont have private and public variable/methods in ES2016
+// we need to use typescript for private/public
+
+
+
+// 4 Pillars of OOP
+
+// Encapsulation - > Wrap methods and data together
+// Abtraction -> Hide all complexity in class implemenation
+// Inheritance
+// Polymorphism. -> we can overwrite attack in Elf. then attack works in two different ways depending on who calls it.
 
